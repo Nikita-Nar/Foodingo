77293364acb58d920d81b780e65a9c52e3458a65
Ela <55469940+Ela01@users.noreply.github.com>
2023-10-09 17:57:45 -0400

    Update App.js (#9)

--- App.js
+++ App.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import StartAssesment from './StartAssesment';
 import LevelMap from './LevelMap';
+import NewbieLevel from './DifficultyLevel/NewbieLevel';
+import NoviceLevel from './DifficultyLevel/NoviceLevel';
+import MidTierLevel from './DifficultyLevel/MidTierLevel';
+import AdvancedLevel from './DifficultyLevel/AdvancedLevel';
+import ExpertLevel from './DifficultyLevel/ExpertLevel';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const logoImage = require('./assets/DingoLogo.jpg');
-const BackgroundImg = require('./assets/StartingBG.jpg');
+const logoImage = require('./DingoLogo.jpg');
 const Stack = createNativeStackNavigator();
 
 
 function HomeScreen({ navigation }) {
   return (
-    <ImageBackground source={BackgroundImg} style={styles.container}>
-      <View style={styles.container}>
-        <Image source={logoImage} style={styles.logo}/>
-        <Text style={styles.headerText}>Welcome</Text>
-        <Text style={styles.chefText}>CHEF</Text>
-        <Text style={styles.smallText}>Let's assess your culinary skills!</Text>
-        <Button 
-          title = "Start Assessment"
-          onPress={() => navigation.navigate('StartAssesment')}
-        />
-        <Button  
-          title = "Skip Assessment"
-          onPress={() => navigation.navigate('LevelMap')}
-        />
-        <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Your Culinary Journey Starts Here</Text>
+      <Image source={logoImage} style={styles.logo}/>
+      <Button  
+        title = "Start Assesment"
+        onPress={() => navigation.navigate('StartAssesment')}
+      />
+      <Button  
+        title = "Skip Assesment"
+        onPress={() => navigation.navigate('LevelMap')}
+      />
+      <StatusBar style="auto" />
 
-      </View>
-    </ImageBackground>
+    </View>
   );
 }
 
@@ -57,7 +57,32 @@ export default function App() {
         <Stack.Screen 
           name="LevelMap"
           component={LevelMap}
-          options={{title: 'Level Map'}}
+          options={{title: 'Level Map', headerBackVisible: false}}
+        />
+        <Stack.Screen 
+          name="NewbieLevel"
+          component={NewbieLevel}
+          options={{title: 'Newbie Level'}}
+        />
+        <Stack.Screen 
+          name="NoviceLevel"
+          component={NoviceLevel}
+          options={{title: 'Novice Level'}}
+        />
+        <Stack.Screen 
+          name="MidTierLevel"
+          component={MidTierLevel}
+          options={{title: 'Mid-Tier Level'}}
+        />
+        <Stack.Screen 
+          name="AdvancedLevel"
+          component={AdvancedLevel}
+          options={{title: 'Advanced Level'}}
+        />
+        <Stack.Screen 
+          name="ExpertLevel"
+          component={ExpertLevel}
+          options={{title: 'Expert Level'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -68,38 +93,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'lightsalmon',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    paddingTop: '30%'
   },
   headerText: {
     color: 'white',
-    fontSize: 70,
-    letterSpacing: 2,
-    fontFamily: 'Times New Roman',
-    textAlign: 'center',
-  },
-  chefText: {
-    flex: 0.2,
-    color: 'white',
-    fontSize: 40,
-    letterSpacing: 7,
-    fontFamily: 'Avenir-Roman',
-    textAlign: 'center',
-  },
-  smallText: {
-    flex: 0.3,
-    width: 220,
-    color: '#cc1b12',
-    fontSize: 29,
-    letterSpacing: 1,
-    fontFamily: 'Times New Roman',
+    fontSize: 24,
+    // fontWeight: 'bold',
     textAlign: 'center',
   },
   logo: {
-    width: 170,
-    height: 170, 
-    marginTop: 30,
-    marginBottom: 50,
+    width: 300,
+    height: 300, 
+    marginTop: 50,
     borderRadius: 150,
   },
 });
