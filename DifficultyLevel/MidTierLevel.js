a0fbe9944f3267c9a1e0b361399801892033d559
Ela <55469940+Ela01@users.noreply.github.com>
2023-10-09 13:35:47 -0400

    Ela.wieczorek (#6)
    * Added folder structure
    
    * Fixed Directory Tree
    
    * Test
    
    * 2 New Files

--- DifficultyLevel/MidTierLevel.js
+++ DifficultyLevel/MidTierLevel.js
@@ -1,16 +1,137 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const MidTierLevel = () => {
+// #1: Make app container a ScrollView.
+// #2: Have only one checkbox checked at a time.
+
+
+function MyCheckbox() {
+  const [checked, setChecked] = useState(false);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>This is Mid-Tier Level</Text>
-    </View>
+    <Pressable
+      style={[styles.checkboxBase, checked && styles.checkboxChecked]}
+      onPress={() => setChecked(!checked)}>
+      {checked && <Ionicons name="checkmark" size={24} color="white" />}
+    </Pressable>
   );
-};
-export default MidTierLevel;
+}
+
+export default function MidTierLevel() {
+  return (
+    <View style={styles.appContainer}>
+
+      <Text style={styles.appTitle}>Checkbox Example</Text>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`one`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`two`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`three`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`four`}</Text>
+      </View>
+
+      <Text style={styles.appTitle}>Checkbox Example</Text>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`one`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`two`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`three`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`four`}</Text>
+      </View>
+
+      <Text style={styles.appTitle}>Checkbox Example</Text>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`one`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`two`}</Text>
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`three`}</Text> 
+      </View>
+
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`four`}</Text>
+      </View>
+
+    </View>
+
+
+
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  checkboxBase: {
+    width: 24,
+    height: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#ff0000',
+    backgroundColor: 'transparent',
+  },
+  checkboxChecked: {
+    backgroundColor: '#ff0000',
+  },
+  appContainer: {
+    backgroundColor: 'lightsalmon',
+    flex: 1,
+    alignItems: 'left',
+    justifyContent: 'center',
+    
+  },
+  appTitle: {
+    marginVertical: 16,
+    fontFamily: 'Avenir-Roman',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 10,
+  },
+  checkboxLabel: {
+    marginLeft: 8,
+    fontWeight: 500,
+    fontSize: 18,
+    paddingRight: 16,
+  },
+});
