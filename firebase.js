5fead5293c9b9b3de454a74104ec21c124b43235
LuckyFlower2995 <rufinai@MrSambos-MacBook-Pro.local>
2023-10-23 13:52:05 -0400

    commit firebase sdks

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCo-tqt5WWhZv_ckJXrJgU30WxjFw5ZjLY",
+  authDomain: "fir-setup-55360.firebaseapp.com",
+  projectId: "fir-setup-55360",
+  storageBucket: "fir-setup-55360.appspot.com",
+  messagingSenderId: "965439790089",
+  appId: "1:965439790089:web:29514bd048e2eeb5722cd8"
+};
+
+const app = initializeApp(firebaseConfig); // Initalizes app firebase config
